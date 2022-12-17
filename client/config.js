8a130d635dbe1d774a5b2e4c8981c7e28894d4cf
Big Fat Grub <115567810+BigFatGrub@users.noreply.github.com>
2022-12-16 20:38:26 -0600

    Update config.js

--- client/config.js
+++ client/config.js
@@ -4,7 +4,5 @@ window.servers = [{
   name: " FFA",
 }]
 // Year, Month, Full Date
-window.changelogDate = []; // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V1.0","Machine Gun reload is now slower", "Penetration now does stuff", "Added autofire", "You can now upgrade your stats/skills with numbers in your key", "Fixed a bug, where all of the bullet healths were the same, causing bullets to be extremely weak", "Fixed Destroyer and Annihilator design (Suggested by Oblivion)", "Nerfed Assassin, Sniper and Ranger spray", "Buffed movement speed skill", "Added Negev", "Fixed a circle rendering issue", "Adjusted the UI a lot", "Adjusted the UI size to be better for every screen", "Fixed a bug where the tanks didnt grow on death animation", "Pressing enter on death screen now redirects you to the menu", "Changed connecting screen background to be a grid", "Finally implemented reload delay, no more cursed tanks"];
-
-
+window.changelogDate = [Date.now()]; // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
+window.changelog = ["Welcome to PolyTonk V1.02", "Increased map size","Player of the week: Nintendo 64"];
