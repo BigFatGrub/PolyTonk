442ffa47140db23fc05c2865b91bb0317040ccbf
Big Fat Grub <115567810+BigFatGrub@users.noreply.github.com>
2023-01-29 00:24:12 -0600

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,8 +1,10 @@
 window.beta = false;
 window.servers = [{
   wsLink: (window.top.location.protocol == "https:" ? "wss://" : "ws://") + window.top.location.host + "/server",
-  name: " FFA",
+  name: "ffa",
 }]
 // Year, Month, Full Date
 window.changelogDate = [Date.now()]; // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V1.02", "Increased map size","Player of the week: Nintendo 64"];
+window.changelog = ["Welcome to PolyTonk V1.03", "Increased map size by uhhh alot lol", "drecreased all snipers viewrange including the railgun", "Added new tank: Shotty", "Decreased penatration on the Pentashot", "Player of the week: SovietStates0", "Soviet was here😎", "Hey so was I bro"];
+
+
